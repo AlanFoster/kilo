f52ef58e5e9063fe7c4c48cb3999e11330a028b7
Alan Foster <alan@alanfoster.me>
2017-04-06 23:42:33 +0100

    Print read characters, with a read timeout set, as well as ignoring various signals / key combinations

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -18,7 +20,14 @@ void restoreTerminalMode() {
 void enableRawMode() {
   struct termios raw;
   tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO | ICANON);
+ 
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); 
+  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -28,9 +37,17 @@ int main() {
   enableRawMode();
   atexit(restoreTerminalMode);
 
-  char c;
-
-  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+    if (isprint(c)) {
+      printf("%d ('%c')\r\n", c, c);
+    } else { 
+      printf("%d\r\n", c);
+    }
+ 
+    if (c == 'q') break;   
+  }
 
   return 0;
 }
