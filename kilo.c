e8461adc517ede7dc3c7568b2f3143b94a02d72f
Alan Foster <alan@alanfoster.me>
2017-04-08 00:01:53 +0100

    Introduce string buffer to write once to screen

--- kilo.c
+++ kilo.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -19,11 +20,39 @@ struct editorConfig {
 
 struct editorConfig editor_config;
 
+
+struct outputBuffer {
+  char *start;
+  int length;
+};
+
+#define EMPTY_OUTPUT_BUFFER {NULL, 0}
+
+void appendBuffer(struct outputBuffer *output_buffer, const char *s, int length) {
+  char *new_start = realloc(output_buffer->start, output_buffer->length + length);
+
+  if (new_start == NULL) return;
+  memcpy(&new_start[output_buffer->length], s, length);
+  output_buffer->start = new_start;
+  output_buffer->length += length;
+}
+
+void clearBuffer(struct outputBuffer *output_buffer) {
+  free(output_buffer->start);
+}
+
 void clearScreen() {
   // Clear Screen
   write(STDOUT_FILENO, "\x1b[2J", 4);
   // Place cursor top left
   write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void clearScreenWithBuffer(struct outputBuffer *output_buffer) {
+  // Clear Screen
+  appendBuffer(output_buffer, "\x1b[2J", 4);
+  // Place cursor top left
+  appendBuffer(output_buffer, "\x1b[H", 3);
 }
 
 void die(const char *s) {
@@ -51,8 +80,8 @@ void restoreTerminalMode() {
 void enableRawMode() {
   struct termios raw;
   tcgetattr(STDIN_FILENO, &raw);
- 
-  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); 
+
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_iflag &= ~(ICRNL | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
@@ -67,8 +96,8 @@ void enableRawMode() {
 
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
-  
-  if (isFailure(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) || ws.ws_col == 0)) {
+
+  if (isFailure(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)) || ws.ws_col == 0) {
     return -1;
   }
 
@@ -77,21 +106,27 @@ int getWindowSize(int *rows, int *cols) {
   return 0;
 }
 
-void drawRows() {
+void drawRows(struct outputBuffer *output_buffer) {
   int y;
   for (y = 0; y < editor_config.screen_rows; y++) {
-    write(STDOUT_FILENO, "\x1b[31m", 5);
-    
+    appendBuffer(output_buffer, "\x1b[31m", 5);
+
     if (y < editor_config.screen_rows - 1) {
-      write(STDOUT_FILENO, "~\r\n", 3);
+      appendBuffer(output_buffer, "~\r\n", 3);
     }
   }
 }
 
 void refreshScreen() {
-  clearScreen();
-  drawRows();
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct outputBuffer output_buffer = EMPTY_OUTPUT_BUFFER;
+
+  clearScreenWithBuffer(&output_buffer);
+  drawRows(&output_buffer);
+  appendBuffer(&output_buffer, "\x1b[H", 3);
+
+  write(STDOUT_FILENO, output_buffer.start, output_buffer.length);
+
+  clearBuffer(&output_buffer);
 }
 
 char readKey() {
@@ -103,14 +138,14 @@ char readKey() {
       die("readKey failure");
     }
   } while (!hasInput(readStatusCode));
-  
+
   return c;
 }
 
 void handleKeypress(char key) {
   switch (key) {
     case CTRL_KEY('q'):
-     clearScreen(); 
+     clearScreen();
      exit(0);
      break;
   }
